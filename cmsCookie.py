2623482655f3891f97f98207123ef5a464de04e0
Linus Jäger <34212061+linus307@users.noreply.github.com>
2022-11-28 09:37:53 +0100

    Update cmsCookie.py

--- cmsCookie.py
+++ cmsCookie.py
@@ -1,19 +1,26 @@
+#!/usr/bin/env python3
+
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.firefox.options import Options
-import re
+from selenium.webdriver.chrome.options import Options
+import re, secretstorage
 
-username="username"
-password="password"
+connection = secretstorage.dbus_init()
+collection = secretstorage.get_default_collection(connection)
+
+username = ""
+password = [x for x in collection.search_items({'name':'cms_password'})][0].get_secret().decode('utf-8')
+
+connection.close()
 
 options = Options()
 options.add_argument("--headless")
-driver = webdriver.Firefox(options=options)
+driver = webdriver.Chrome(options=options)
 
 driver.get("https://cms.sic.saarland/system/users/login")
-driver.find_element_by_xpath('//*[@id="UserUsername"]').send_keys(username)
-driver.find_element_by_xpath('//*[@id="UserPassword"]').send_keys(password)
-driver.find_element_by_xpath('/html/body/div[1]/div[1]/form/button').click()
+driver.find_element("xpath", '//*[@id="UserUsername"]').send_keys(username)
+driver.find_element("xpath", '//*[@id="UserPassword"]').send_keys(password)
+driver.find_element("xpath", '/html/body/div[1]/div[1]/form/button').click()
 
 WebDriverWait(driver=driver, timeout=10).until(
     lambda x: x.execute_script("return document.readyState === 'complete'")
